24f5904d4bb7eb75c1bbcfe58f423cbaea5df175
Unhero767 <heroiamnot5@gmail.com>
2026-03-28 17:40:36 -0500

    [CORE_DOGMA] Initialize Tier 1 Sovereign Command origin

---
+++ tier1_init.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# [CORE_DOGMA] Enforce Base Validation Matrix
+echo -e "[ [1;36m◦A STABILIZED[0m ] Magisterial tether secured."
+echo "export MAGISTERIUM_ACTIVE=true" > ~/.magisterium_rc
